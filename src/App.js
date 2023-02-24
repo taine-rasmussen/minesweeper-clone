a9d7cdc215b8158b642a893abe16ca7ba88efb2f
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-02-24 20:01:08 +0000

    ?

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
-
+import { useState, useEffect } from 'react'
+import './game.css';
 
 function App() {
+
+  const [gameboard, setGameboard] = useState([])
+
+  const rows = 10
+  const cols = 10
+
+
+  const createGameboard = () => {
+    let game = []
+    for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < cols; j++) {
+        game.push([i, j])
+      }
+    };
+    setGameboard(game)
+  };
+
+  useEffect(
+    () => {
+      createGameboard()
+    }, []
+  );
+
   return (
     <div className="App">
+      <div className='gameboard'>
+        {gameboard.map((cell, i) => {
+          return (
+            <div className='cell' key={cell}>
+              {cell}
+            </div>
+          )
+        })}
+      </div>
     </div>
   );
 }
